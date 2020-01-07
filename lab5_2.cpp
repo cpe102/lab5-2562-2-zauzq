692d43bc3578d924817933db33a20cf9c72e787c
neokarn <34811101+neokarn@users.noreply.github.com>
2020-01-07 14:46:03 +0700

    Initial commit

---
+++ lab5_2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+
+
+int main(){	
+	cout << mixText("AAA","BBB") << "\n";
+	cout << mixText("Hello","World") << "\n";
+	cout << mixText("SOTUS","CHEER") << "\n";
+	cout << mixText("1234","5678") << "\n";
+	cout << mixText("6","9") << "\n";
+	cout << mixText("XXXXXXX","YYY") << "\n";	
+	cout << mixText("Y","XX") << "\n";	
+	
+	return 0;	
+}
