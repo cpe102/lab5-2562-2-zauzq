380bc872be8111368dd4172631732b9e029ba6ca
lab302-24 <lab302-24@labeng.local>
2020-01-07 16:25:57 +0700

    Done

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -1,24 +1,37 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
 	int N;
 	cout << "Enter the number of student: ";
 	cin >> N;
-	
 	string name[N];
 	int age[N];
-	cout << "Name of student = ";
-	cout << "Age of student = ";
-	
+
+	int i=0;
+	while(i < N){
+	cout << "Name of student "<< i+1 <<" =";
+	cin >> name[i];
+	cout << "Age of student " << i+1<<" =";
+	cin >> age[i];
+	i++;
+	}
+
+	i = 0;
 	int key;
 	cout << "--------------------------------------\n";
 	cout << "Enter an age to search: ";
 	cin >> key;
 	cout << "--------------------------------------\n";
+	while (i < N){
+		if(age[i] == key){
+			cout << name[i] << "\n";
+		}
+		i++;
+	}
 	
 
-	
 	cout << "--------------------------------------\n";
 	
 	return 0;
--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,7 +1,23 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
-
+string mixText(string s1,string s2){
+	int L1 = s1.size();
+	int L2 = s2.size();
+	int L = L1+L2;
+	string s ="";
+	if(L1 != L2){
+		cout << "E";
+	}else{
+		int i = 0;
+		while(i < L){
+			s = s + s1[i] + s2[i];
+			i++; 
+		}
+		return s;
+	}
+}
 
 
 int main(){	
